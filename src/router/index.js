43fa70fa5dc5d5e5a46a12acab167973e25234e7
Muhammad Reza <muhammadreza161@gmail.com>
2022-07-28 19:31:39 +0700

    create movie project

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MovieDetail from '../views/MovieDetail.vue'
 
 const routes = [
   {
@@ -7,14 +8,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+{
+    path: '/movie/:id',
+    name: 'Movie Detail',
+    component: MovieDetail
+} 
 ]
 
 const router = createRouter({
